75725d5336286006f54fedc3c13a7da4780a5e7a
Marvin Kelly <mkellythedon@gmail.com>
2012-03-29 13:17:26 +0100

    new Readme file for TestClient.

---
+++ TestClient/TestClient/TestClient/HttpCompressionModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Compression;
+using System.Web;
+
+namespace TestClient
+{
+    public class HttpCompressionModule : IHttpModule
+    {
+
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication application)
+        {
+            application.BeginRequest +=
+                (new EventHandler(this.Application_BeginRequest));
+
+        }
+
+        private void Application_BeginRequest(Object source, EventArgs e)
+        {
+            HttpContext context = HttpContext.Current;
+            context.Request.Filter = new GZipStream(context.Request.Filter, CompressionMode.Compress);
+            HttpContext.Current.Response.AppendHeader("Content-encoding", "gzip");
+            HttpContext.Current.Response.Cache.VaryByHeaders["Accept-encoding"] = true;
+        }
+
+    }
+}
